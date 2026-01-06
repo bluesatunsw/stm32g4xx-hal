652ade2e5c78df4bd8f63acaa3058b018e541bb0
Evan La Fontaine <navelafontaine@gmail.com>
2026-01-06 19:51:39 +1100

    Add exnihilate function to RCC

--- src/rcc/mod.rs
+++ src/rcc/mod.rs
@@ -530,6 +530,12 @@ pub trait RccExt {
 
     /// Constrains the `RCC` peripheral and apply clock configuration
     fn freeze(self, rcc_cfg: Config, pwr_config: PowerConfiguration) -> Rcc;
+
+    /// Assume that the `RCC` peripheral has been constrained in a way we are not
+    /// aware of and assert that it has a particular clock configuration
+    /// 
+    /// SAFETY: the RCC must have been externally configured to yield these clocks
+    unsafe fn exnihilate(self, clocks: Clocks) -> Rcc;
 }
 
 impl RccExt for RCC {
@@ -542,6 +548,13 @@ impl RccExt for RCC {
 
     fn freeze(self, rcc_cfg: Config, pwr_config: PowerConfiguration) -> Rcc {
         self.constrain().freeze(rcc_cfg, pwr_config)
+    }
+
+    unsafe fn exnihilate(self, clocks: Clocks) -> Rcc {
+        Rcc {
+            rb: self,
+            clocks: clocks,
+        }
     }
 }
 
